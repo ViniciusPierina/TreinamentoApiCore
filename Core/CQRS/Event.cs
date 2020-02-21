c82d971f66deebd49bd5ff1dc39c7462ae215ffe
Vinicius <vini.pierina@gmail.com>
2020-02-21 14:19:52 -0300

    Configurating the Core

---
+++ Core/CQRS/Event.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.CQRS
+{
+    public class Event : Message
+    {
+    }
+}
---
+++ Core/CQRS/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.CQRS
+{
+    public abstract class Message
+    {
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+        public Guid AggregateId { get; set; }
+        public string MessageType { get; set; }
+        public DateTime DateTimeStamp { get; set; }
+    }
+}
