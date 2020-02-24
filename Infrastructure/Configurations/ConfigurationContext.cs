b2e47da751cd2e68ba85061d1f899010774c0301
Vinicius <vini.pierina@gmail.com>
2020-02-24 15:27:19 -0300

    Configurating the Infrastructure without the migrations

---
+++ Infrastructure/Configurations/ConfigurationContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configurations
+{
+    public sealed class ConfigurationContext : DbContext
+    {
+        public ConfigurationContext(DbContextOptions<ConfigurationContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/GameRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.Configurations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Repositories
+{
+    public class GameRepository : Repository<Game>, IGameRepository
+    {
+        private readonly ConfigurationContext configurationContext;
+
+        public GameRepository(ConfigurationContext context) : base(context)
+        {
+            configurationContext = context;
+        }
+
+        public IEnumerable<Game> FindGameByReleaseYear(int year)
+        {
+            return configurationContext.Games.Where(game => game.ReleaseYear == year);
+        }
+    }
+}
