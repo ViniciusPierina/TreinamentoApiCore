162f9c6bfb2a4bd32dab253fa14c060a84fb861c
Vinicius <vini.pierina@gmail.com>
2020-02-27 13:16:34 -0300

    Configurating the Entry Points

---
+++ App.WebAPI/Controllers/GameController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.DTOs;
+using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<GameDTO>> GetAll()
+        {
+            return Ok(_gameService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<GameDTO> GetById(Guid id)
+        {
+            var game = _gameService.Get(id);
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CreateGameDTO createGameDTO)
+        {
+            try
+            {
+                _gameService.Save(createGameDTO);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                string errors = e.Message;
+                return ValidationProblem(new ValidationProblemDetails()
+                {
+                    Type = "Model Validation Error",
+                    Detail = errors
+                });
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] UpdateGameDTO updateGameDTO)
+        {
+            try
+            {
+                _gameService.Update(updateGameDTO);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                string errors = e.Message;
+                return ValidationProblem(new ValidationProblemDetails()
+                {
+                    Type = "Model Validation Error",
+                    Detail = errors
+                });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            try
+            {
+                _gameService.Delete(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                string errors = e.Message;
+                return ValidationProblem(new ValidationProblemDetails()
+                {
+                    Type = "Cannot delete",
+                    Detail = errors
+                });
+            }
+        }
+
+        public ActionResult<IEnumerable<GameDTO>> FindGameByReleaseYear(int year)
+        {
+            return Ok(_gameService.FindGameByReleaseYear(year));
+        }
+    }
+}
--- App.WebAPI/Startup.cs
+++ App.WebAPI/Startup.cs
@@ -1,13 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using Core.CQRS;
+using Domain.Commands;
+using Domain.Handlers.Commands;
+using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.Bus;
+using Infrastructure.Configurations;
+using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Services.Mappings;
+using Services.Services;
+using Services.Services.Interfaces;
 
 namespace App.WebAPI
 {
@@ -23,6 +34,32 @@ namespace App.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICityService, CityService>();
+            services.AddScoped<IGameService, GameService>();
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<IRepository<City>, Repository<City>>();
+
+            services.AddScoped<IHandler<CreateCityCommand>, CityCommandHandler>();
+            services.AddScoped<IHandler<UpdateCityCommand>, CityCommandHandler>();
+            services.AddScoped<IHandler<RemoveCityCommand>, CityCommandHandler>();
+
+            services.AddScoped<IHandler<CreateGameCommand>, GameCommandHandler>();
+            services.AddScoped<IHandler<UpdateGameCommand>, GameCommandHandler>();
+            services.AddScoped<IHandler<RemoveGameCommand>, GameCommandHandler>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<IBus, InMemoryBus>();
+
+            ConfigureAutomapper(services);
+
+            ConfigureSwagger(services);
+
+            services.AddDbContext<ConfigurationContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             services.AddRazorPages();
         }
 
@@ -40,6 +77,8 @@ namespace App.WebAPI
                 app.UseHsts();
             }
 
+            ConfigureSwaggerUi(app);
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -50,6 +89,35 @@ namespace App.WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+            });
+        }
+
+        private static void ConfigureAutomapper(IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfiles());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestNetCore API", Version = "v1" });
+            });
+        }
+
+        private static void ConfigureSwaggerUi(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestNetCore");
             });
         }
     }
